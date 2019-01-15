f1db1cdbc12c26736361bf151bd1e978b444bc96
Samuel Lopes Dantas Machado <samuel.l.d.machado@gmail.com>
2019-01-15 20:05:07 -0200

    Monstro simples

---
+++ Assets/Scripts/MonsterCtrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCtrl : MonoBehaviour {
+
+	public float speed = -2f;
+	
+	Rigidbody2D rb;
+	SpriteRenderer sr;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < GM.instance.yMinLive){
+			Destroy(gameObject);
+		}
+	}
+
+	void Move(){
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+	}
+
+	void OnCollisonEnter2D(Collision2D other){
+		if(!other.gameObject.CompareTag("Player")){
+			Flip();
+		}
+	}
+
+	void Flip(){
+		speed = -speed;
+		if (speed > 0){
+			sr.flipX = true;
+		}
+		else if(speed < 0){
+			sr.flipX = false;
+		}
+	}
+}
