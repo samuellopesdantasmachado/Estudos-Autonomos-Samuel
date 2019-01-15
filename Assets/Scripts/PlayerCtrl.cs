5a75db9755f7ab9c1bec11b18738b3366a4cab6f
Samuel Lopes Dantas Machado <samuel.l.d.machado@gmail.com>
2019-01-15 02:16:52 -0200

    Double jump

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -28,6 +28,8 @@ using UnityEngine;
 
 		public bool isGrounded;
 		public LayerMask whatIsGround;
+		bool canDoubleJump = false;
+		public float delayForDoubleJump = 0.2f;
 
 		// Use this for initialization
 		void Start () {
@@ -94,7 +96,21 @@ using UnityEngine;
 			   isJumping = true;
 			   rb.AddForce(new Vector2(0f, jumpSpeed));
 			   anim.SetInteger("State", 1);
+
+			   Invoke("EnableDoubleJump", delayForDoubleJump);
 			}
+
+            if (canDoubleJump && !isGrounded){
+				rb.velocity = Vector2.zero;
+				rb.AddForce(new Vector2(0f, jumpSpeed));
+				anim.SetInteger("State", 1);
+				canDoubleJump = false;
+			}
+
+		}
+
+		void EnableDoubleJump(){
+			canDoubleJump = true;
 		}
 
 		void OnCollisionEnter2D(Collision2D other){
