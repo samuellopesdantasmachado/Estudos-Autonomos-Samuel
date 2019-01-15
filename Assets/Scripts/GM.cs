895546875d83dae10fcb992d0a0863bc50f7f270
Samuel Lopes Dantas Machado <samuel.l.d.machado@gmail.com>
2019-01-15 03:39:31 -0200

    Die & respawn

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+	public static GM instance = null;
+
+    public float yMinLive = -10f;
+	public Transform spawnPoint;
+	public GameObject playerPrefab;
+
+	
+	PlayerCtrl player;
+
+	public float timeToRespawn = 2f;
+
+	void Awake(){
+		if (instance == null){
+			instance = this;
+		}
+		else if (instance != this){
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start () {
+		if (player == null){
+			RespawnPlayer();
+		}
+	}
+	
+	void Update () {
+		if (player == null){
+			GameObject obj = GameObject.FindGameObjectWithTag("Player");
+			if (obj != null){
+				player = obj.GetComponent<PlayerCtrl>();
+			}
+		}
+	}
+
+	public void RespawnPlayer(){
+		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+
+	public void KillPlayer(){
+		if (player != null){
+			Destroy(player.gameObject);
+			Invoke("RespawnPlayer", timeToRespawn);
+		}
+	}
+}
--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -45,6 +45,10 @@ using UnityEngine;
 		// Update is called once per frame
 		void Update () {
 
+			if(transform.position.y < GM.instance.yMinLive){
+				GM.instance.KillPlayer();
+			}
+
 			isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y), new Vector2(feetWidth, feetheight), 360.0f, whatIsGround);
 	
 			float horizontalInput=Input.GetAxisRaw ("Horizontal"); // -1: esquerda / 1: direta //
